4ba69acc67acc4567865729f1e2573b45b62c55c
VenkateshwaraStutzen <venkateshwara@stutzen.me>
2025-04-29 13:50:24 +0530

    smock range slider

---
+++ smock-range-slider/script.js
@@ -0,0 +1,69 @@
+const $ = (s, o = document) => o.querySelector(s);
+
+var interval;
+
+let slider = $(".slider"),
+  input = $("input", slider),
+  random = (min, max) => min + Math.random() * (max - min),
+  between = (min, max, percent) => max - (max - min) * (1 - percent),
+  create = (slider) => {
+    let percent = slider.handle.dataset.percent,
+      position = slider.handle.dataset.position,
+      div = document.createElement("div");
+
+    div.classList.add("smoke");
+    slider.range.appendChild(div);
+
+    gsap.set(div, {
+      x: position,
+      y: -20,
+    });
+
+    gsap
+      .timeline()
+      .to(div, {
+        scale: random(between(0.15, 1, percent), between(0.4, 2, percent)),
+        duration: between(0.8, 1.2, percent),
+      })
+      .to(div, {
+        scale: 0,
+        duration: between(0.3, 0.5, percent),
+      });
+
+    gsap.to(div, {
+      duration: between(0.3, 0.4, percent),
+      opacity: 1,
+    });
+
+    gsap.to(div, {
+      duration: between(2, 3, percent),
+      y: random(between(-40, -200, percent), between(-70, -320, percent)),
+      x:
+        random(between(-20, -90, percent), between(20, 90, percent)) +
+        parseInt(position),
+      onComplete() {
+        div.remove();
+      },
+    });
+  };
+
+rangesliderJs.create(input, {
+  onInit(value) {
+    this.handle.dataset.value = value;
+  },
+  onSlideStart(value, percent, position) {
+    this.handle.classList.add("active");
+    interval = setInterval(() => {
+      create(this);
+    }, 50);
+  },
+  onSlide(value, percent, position) {
+    this.handle.dataset.value = value;
+    this.handle.dataset.percent = percent;
+    this.handle.dataset.position = position;
+  },
+  onSlideEnd() {
+    this.handle.classList.remove("active");
+    clearInterval(interval);
+  },
+});
