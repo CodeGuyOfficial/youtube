0a8eb41062a554fb9eab56215ef1cf2ab3cad862
VenkateshwaraStutzen <venkateshwara@stutzen.me>
2025-04-30 16:49:32 +0530

    checkbox

---
+++ paper-and-pencil-checkbox/script.js
@@ -0,0 +1,12 @@
+
+
+
+document.addEventListener("DOMContentLoaded",function(){
+	document.querySelector("form").addEventListener("click",e => {
+		let checkboxCL = e.target.classList,
+			pState = "pristine";
+
+		if (checkboxCL.contains(pState))
+			checkboxCL.remove(pState);
+	});
+});
